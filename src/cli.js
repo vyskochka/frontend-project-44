f04f7bf7247056df8a3023f2653cdd50eff99d5d
AndreyBasuda <zaluckaev.a@yandex.ru>
2024-02-01 13:34:59 +0300

    task 3

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+const greeting = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ')
+    return console.log('Hello, ' + name + '!');
+};
+export default greeting;
